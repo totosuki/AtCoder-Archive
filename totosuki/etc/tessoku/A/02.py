52d2efea9a5955155d5c9e08c7a1595c08d75ee9
totosuki <e231kari@gmail.com>
2023-09-19 14:45:35 +0900

    鉄則本 A02 Code Golfやった

--- totosuki/etc/tessoku/A/02.py
+++ totosuki/etc/tessoku/A/02.py
@@ -7,3 +7,22 @@ for a in A:
     answer = "Yes"
 
 print(answer)
+
+# Code Golf
+# print(*["Yes"if x in[input().split()]else"No"for n,x in[input().split()]])
+
+# [print("Yes"if x in[input().split()]else"No")for n,x in[input().split()]]
+
+# for n,x in[input().split()]:print("Yes"if x in[input().split()]else"No")
+
+# for n,x in[input().split()]:print("Yes"if x in input().split()else"No")
+
+# for n,x in input().split():print("Yes"if x in input().split()else"No")
+
+# for n,x in input().split():print("NYoe s"[x in input().split()::2])
+
+# for x,y in[open(0)]:print("NYoe s"[x.split()[1]in y.split()::2])
+
+# for x,y in[map(str.split,open(0))]:print("NYoe s"[x[1]in y::2])
+
+# print("NYoe s"[input().split()[1]in input().split()::2])
