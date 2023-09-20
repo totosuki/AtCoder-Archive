fb2d1a9c2afd16611283fe6759496934d1c512a8
totosuki <e231kari@gmail.com>
2023-09-20 15:05:48 +0900

    鉄則本 A19 AC

---
+++ totosuki/etc/tessoku/A/19.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.buffer.readline
+
+N, W = map(int, input().split())
+WV = [[0,0]] + [list(map(int, input().split())) for _ in range(N)]
+
+dp = [[-float("inf")] * (W+1) for _ in range(N+1)]
+dp[0][0] = 0
+
+for i in range(1, N+1):
+  for j in range(W+1):
+    if j + WV[i][0] <= W:
+      dp[i][j] = max(dp[i-1][j], dp[i][j])
+      dp[i][j+WV[i][0]] = max(dp[i-1][j]+WV[i][1], dp[i][j+WV[i][0]])
+    else:
+      dp[i][j] = max(dp[i-1][j], dp[i][j])
+
+print(max(dp[N]))
