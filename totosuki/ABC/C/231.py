8bdac0058e1f91746b3f73f14a07ab1b12db9423
totosuki <e231kari@gmail.com>
2023-10-21 12:57:54 +0900

    ABC 231 ABC　再ACした

--- totosuki/ABC/C/231.py
+++ totosuki/ABC/C/231.py
@@ -1,22 +1,22 @@
-import sys
-input = sys.stdin.buffer.readline
+# 117byte
+# 121byte
 
-N, Q = map(int, input().split())
-A = list(map(int, input().split()))
-A.sort()
 
-def binary_search(x):
-  left = -1
-  right = N
+from bisect import*
+N,A,*X=map(str.split,open(0))
+A=sorted(map(int,A))
+for x in X:print(int(N[0])-bisect_left(A,int(*x)))
 
-  while right - left > 1:
-    mid = (left + right ) // 2
-    if A[mid] < x: left = mid
-    else: right = mid
-  
-  return right
+# 120byte
+# from bisect import*
+# N,A,*X=open(0)
+# N,_=N.split()
+# for x in X:print(int(N)-bisect_left(sorted(map(int,A.split())),iant(x)))
 
-for _ in range(Q):
-  x = int(input())
-  rslt = binary_search(x)
-  print(N - rslt)
+# N, Q = map(int, input().split())
+# A = list(map(int, input().split()))
+# A.sort()
+
+# for _ in range(Q):
+#   X = int(input())
+#   print(N - bisect_left(A, X))
