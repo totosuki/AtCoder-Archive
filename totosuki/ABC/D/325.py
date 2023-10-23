e96b1b50e86fd25d1469ecbdad97189566d88e60
totosuki <e231kari@gmail.com>
2023-10-23 11:15:46 +0900

    ABC 325 D AC

--- totosuki/ABC/D/325.py
+++ totosuki/ABC/D/325.py
@@ -1,30 +1,47 @@
+import sys
+input = sys.stdin.buffer.readline
+
+import heapq
+
 N = int(input())
 data = []
 
 for _ in range(N):
-  T, D = map(int, input().split())
-  data.append((T, T + D))
+  t, d = map(int, input().split())
+  data.append((t, t+d))
 
-data.sort(key = lambda x: (x[1], x[0]))
-time = 0
+data.sort(key = lambda x: x[0])
+
+nt = data[0][0]
 cnt = 0
+i = 1
 
-for item in data:
-  start, fin = item
-  if time <= start:
-    cnt += 1
-    time = start + 1
-  elif time <= fin:
-    cnt += 1
-    time += 1
-  else:
-    continue
+wait = [(data[0][1], data[0][0])]
+heapq.heapify(wait)
+
+while i < N:
+  next_t, next_d = data[i]
+  
+  if len(wait) == 0:
+    nt = next_t
+  
+  while next_t <= nt:
+    heapq.heappush(wait, (next_d, next_t))
+    i += 1
+    if i < N:
+      next_t, next_d = data[i]
+    else:
+      next_t = float("inf")
+      break
+  
+  while len(wait):
+    d, t = heapq.heappop(wait)
+    
+    if nt <= d:
+      nt += 1
+      cnt += 1
+
+    if next_t <= nt:
+      break
 
 print(cnt)
-
-# [0 0 0 0 0 0]
-# [0 0 0 0 0 0]
-# [0 0 0 0 0 0]
-# [0 0 0 0 0 0]
-# [0 0 0 0 0 0]
-# [0 0 0 0 0 0]
