53a807612753deb3fda6d8c0dd602e72af12d551
totosuki <e231kari@gmail.com>
2023-11-10 12:06:05 +0900

    [ABC 193] D AC

--- totosuki/ABC/D/193.py
+++ totosuki/ABC/D/193.py
@@ -1,68 +1,34 @@
-import copy
-from itertools import permutations
+def return_C(S, T, K):
+  C = [K] * 10
+  for s in S: C[s] -= 1
+  for t in T: C[t] -= 1
+  return C
 
-def return_maisuu(L):
-  maisuu = [0] * 10
-  for i in range(4):
-    card = int(L[i])
-    maisuu[card] += 1
-  return maisuu
-
-def return_pettern(L, K, maisuu):
-  pettern = [list(map(int, copy.copy(L[:4]))) for _ in range(9)]
-  pettern = []
-  for num in range(1, 10):
-    pettern.append(list(map(int, copy.copy(L[:4]))) + [num])
-  return pettern
-
-def return_score(L):
-  cnt = [0] * 10
-  for num in L:
-    cnt[num] += 1
+def score(L):
   score = 0
-  for i in range(10):
-    score += i * 10**cnt[i]
-  return score
-
-def can_battle(S, T, K):
   cnt = [0] * 10
-  can = True
-  for s in S:
-    cnt[s] += 1
-  for t in T:
-    cnt[t] += 1
-  for c in cnt:
-    if c > K:
-      can = False
-  return can
-
-def check(S_pettern, T_pettern, K):
-  all_cnt = 0
-  win_cnt = 0
-  for S in S_pettern:
-    for T in T_pettern:
-      if can_battle(S, T, K):
-        all_cnt += 1
-        S_score = return_score(S)
-        T_score = return_score(T)
-        
-        print(f"S : {S}, T : {T}, S_score : {S_score}, T_score : {T_score}")
-        
-        if S_score > T_score:
-          win_cnt += 1
-  return win_cnt, all_cnt
-
+  for num in L: cnt[num] += 1
+  for i in range(1, 10): score += i*(10**cnt[i])
+  return score
 
 def main():
   K = int(input())
-  S = list(input())
-  T = list(input())
-  maisuu_S = return_maisuu(S)
-  maisuu_T = return_maisuu(T)
-  S_pettern = return_pettern(S, K, maisuu_S)
-  T_pettern = return_pettern(T, K, maisuu_T)
-  win_cnt, all_cnt = check(S_pettern, T_pettern, K)
-  print(win_cnt, all_cnt)
+  S = list(map(int, input()[:4]))
+  T = list(map(int, input()[:4]))
+  C = return_C(S, T, K)
+  all_cnt = (9*K-8) * (9*K-9)
+  win_cnt = 0
+  for i in range(1, 10): # S
+    for j in range(1, 10): # T
+      if C[i] == 0 or C[j] == 0: continue
+      if i == j and C[i] <= 1: continue
+      SI = S + [i]
+      TJ = T + [j]
+      if score(SI) > score(TJ):
+        if i == j:
+          win_cnt += C[i] * (C[i]-1)
+        else:
+          win_cnt += C[i] * C[j]
   print(win_cnt / all_cnt)
 
 main()
