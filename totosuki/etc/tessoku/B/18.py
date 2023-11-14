9a91d7a4743ab12f5aeb7d4827f6eadebcef3baf
totosuki <e231kari@gmail.com>
2023-11-14 17:04:44 +0900

    鉄則本 B18 再AC

--- totosuki/etc/tessoku/B/18.py
+++ totosuki/etc/tessoku/B/18.py
@@ -1,30 +1,35 @@
-import sys
-input = sys.stdin.buffer.readline
+def dynamic_programming(N, S, A):
+  dp = [[False]*(S+1) for _ in range(N+1)]
+  dp[0][0] = True
+  for i in range(1, N+1):
+    a = A[i]
+    for j in range(S+1):
+      if j < a:
+        dp[i][j] = dp[i-1][j]
+      else:
+        dp[i][j] = dp[i-1][j] or dp[i-1][j-a]
+  return dp
 
-N, S = map(int, input().split())
-A = [0] + list(map(int, input().split()))
-
-dp = [[False] * (S+1) for _ in range(N+1)]
-dp[0][0] = True
-
-for i in range(1, N+1):
-  for j in range(S+1):
-    if dp[i-1][j]:
-      dp[i][j] = True
-      if j + A[i] <= S:
-        dp[i][j+A[i]] = True
-
-pos = S
-rslt = []
-
-if dp[N][S]:
+def dp_root(N, S, A, dp):
+  root = []
+  j = S
   for i in reversed(range(1, N+1)):
-    if pos - A[i] >= 0:
-      if dp[i-1][pos-A[i]]:
-        pos = pos-A[i]
-        rslt.append(i)
-  rslt.reverse()
-  print(len(rslt))
-  print(*rslt)
-else:
-  print(-1)
+    a = A[i]
+    if j >= a and dp[i-1][j-a]:
+      root.append(i)
+      j -= a
+  return list(reversed(root))
+
+def main():
+  N, S = map(int, input().split())
+  A = list(map(int, input().split()))
+  A = [0] + A
+  dp = dynamic_programming(N, S, A)
+  if dp[N][S]:
+    root = dp_root(N, S, A, dp)
+    print(len(root))
+    print(*root)
+  else:
+    print(-1)
+
+main()
