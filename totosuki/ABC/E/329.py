47a83f24af0853e101de758f06a6e373a8146ce4
totosuki <e231kari@gmail.com>
2023-11-19 15:16:49 +0900

    [ABC 329] E AC

--- totosuki/ABC/E/329.py
+++ totosuki/ABC/E/329.py
@@ -1,33 +1,38 @@
-def dynamic_programming(N, M, S, T):
-  dp = [None] * (N+1)
-  for i in range(1, M+1):
-    if S[i] == T[i]:
-      dp[i] = True
-    else:
-      dp[i] = False
-  print(dp, 1)
-  for i in range(2, N+1):
-    tf_cnt = -1 # True Falseカウント 両方なったら交差してる
-    for j in range(M):
-      if i + j > N: break
-      if S[i+j] == T[j+1]:
-        tf_cnt = 1
-      else:
-        tf_cnt = 0
-      if tf_cnt == 1 and dp[i+j] == True:
-        dp[i+j] = True
-      if tf_cnt == 0 and dp[i+j] == False:
-        dp[i+j] = False
-      if tf_cnt == 1 and dp[i+j] == None:
-        dp[i:i+j+1] = [True] * (j+1)
-    print(dp, i)
-  return dp
+from collections import deque
+
+def check(M,S,T,i):
+  flag = True
+  for j in range(M):
+    flag = flag and (S[i+j] == T[j] or S[i+j] == "#")
+  return flag
+
+def return_deque(N,M,S,T):
+  ok = [True] * N
+  q = deque()
+  for i in range(N-M+1):
+    if check(M,S,T,i):
+      ok[i] = False
+      q.append(i)
+  return q, ok
+
+def solve(N,M,S,T,q:deque,ok):
+  while q:
+    now = q.pop()
+    S[now:now+M] = ["#"] * M
+    for i in range(now-M+1, now+M):
+      if 0 <= i < N-M+1 and ok[i]:
+        if check(M,S,T,i):
+          ok[i] = False
+          q.append(i)
+  return set(S)
 
 def main():
   N, M = map(int, input().split())
-  S = ["_"] + list(input())
-  T = ["_"] + list(input())
-  dp = dynamic_programming(N, M, S, T)
-  print("Yes" if False not in dp[1:] else "No")
+  S = list(input())
+  T = list(input())
+  q, ok = return_deque(N,M,S,T)
+  rslt = solve(N,M,S,T,q,ok)
+  
+  print("Yes" if len(rslt) == 1 and list(rslt)[0] == "#" else "No")
 
 main()
