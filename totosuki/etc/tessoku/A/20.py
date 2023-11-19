0411b7581ed95d363d0359c5fcb80ae7490fdc4f
totosuki <e231kari@gmail.com>
2023-11-19 20:21:50 +0900

    [鉄則本] A20 再AC

--- totosuki/etc/tessoku/A/20.py
+++ totosuki/etc/tessoku/A/20.py
@@ -1,17 +1,19 @@
-import sys
-input = sys.stdin.buffer.readline
+def dynamic_programming(N, M, S, T):
+  dp = [[0]*(M+1) for _ in range(N+1)]
+  for i in range(1, N+1):
+    for j in range(1, M+1):
+      if S[i] == T[j]:
+        dp[i][j] = max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]+1)
+      else:
+        dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+  return dp
 
-S = ["a"] + list(input().decode().rstrip())
-T = ["a"] + list(input().decode().rstrip())
+def main():
+  S = ["_"] + list(input())
+  T = ["_"] + list(input())
+  N = len(S) - 1
+  M = len(T) - 1
+  dp = dynamic_programming(N, M, S, T)
+  print(dp[N][M])
 
-dp = [[0] * (len(T)) for _ in range(len(S))]
-
-for i in range(1, len(S)):
-  for j in range(1, len(T)):
-    Si, Tj = S[i], T[j]
-    if Si == Tj:
-      dp[i][j] = max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]+1)
-    else:
-      dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-
-print(dp[len(S)-1][len(T)-1])
+main()
