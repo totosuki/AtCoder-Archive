098fa5c0b9e18d898e9fa745ced1a08b0dbc4610
totosuki <e231kari@gmail.com>
2023-11-22 16:08:44 +0900

    [鉄則本] B26 再AC

--- totosuki/etc/tessoku/B/26.py
+++ totosuki/etc/tessoku/B/26.py
@@ -1,26 +1,38 @@
 import pickle
 import sys
 
+def check(mid, A, X):
+  return A[mid] > X
+def binary_search(A, X):
+  left = -1
+  right = len(A)
+  while right - left > 1:
+    mid = (left + right) // 2
+    if check(mid, A, X):
+      right = mid
+    else:
+      left = mid
+  return left
+
 def eratosthenes(N):
-  is_prime = [1] * (N//2)
-  is_prime[0] = False
-  sqrt_p = (int(N**0.5) + 1) // 2
-  for i in range(1, sqrt_p):
+  is_prime = [1] * (N+1)
+  for p in range(2, int(N**0.5)+1):
     if not is_prime: continue
-    p = 2 * i + 1
-    for q in range(p*p//2, len(is_prime), p):
+    for q in range(p*p, N+1, p):
       is_prime[q] = 0
   return is_prime
 
 def main():
   N = int(input())
-  with open("./prime.pickle", "rb") as f:
+  with open("./rslt.pickle", "rb") as f:
     L = pickle.load(f)
-  print(2, *[i*2+1 for i in range(N//2) if L[i]], sep = "\n")
+  id = binary_search(L, N)
+  print(*L[:id+1], sep = "\n")
 
 if sys.argv[-1] == "ONLINE_JUDGE":
   prime_list = eratosthenes(1000000)
-  with open("./prime.pickle", "wb") as f:
-    pickle.dump(prime_list, f)
+  rslt = [n for n in range(2, 10**6) if prime_list[n]]
+  with open("./rslt.pickle", "wb") as f:
+    pickle.dump(rslt, f)
 else:
   main()
