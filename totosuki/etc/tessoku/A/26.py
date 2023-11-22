18535532f06ff616d04369ec05ada484d93e8706
totosuki <e231kari@gmail.com>
2023-11-22 14:22:05 +0900

    [鉄則本] A26 再AC

--- totosuki/etc/tessoku/A/26.py
+++ totosuki/etc/tessoku/A/26.py
@@ -1,28 +1,28 @@
-from math import isqrt
+from numpy import ones
 import sys
-input = sys.stdin.buffer.readline
 
-Q = int(input())
-
-def eratosthenes(N):
-  is_prime = [1] * (N//2)
-  is_prime[0] = False
-  sqrt_p = (isqrt(N) + 1) // 2
+def eratosthenes(N : int):
+  is_prime = ones(N//2, dtype = bool)
+  is_prime[0] = 0
+  sqrt_p = (int(N**0.5) + 1) // 2
   for i in range(1, sqrt_p):
-    if not is_prime: continue
+    if not is_prime[i]: continue
     p = 2 * i + 1
-    for q in range(p*p//2, len(is_prime), p):
-      is_prime[q] = 0
+    is_prime[p*p//2::p] = 0
   return is_prime
 
-li = eratosthenes(300000)
+def main():
+  input = sys.stdin.buffer.readline
+  Q = int(input())
+  prime_list = eratosthenes(300000)
+  for _ in range(Q):
+    X = int(input())
+    if X == 2:
+      print("Yes")
+    elif X % 2 == 0:
+      print("No")
+    else:
+      X //= 2
+      print("Yes" if prime_list[X] else "No")
 
-for _ in range(Q):
-  X = int(input())
-  if X == 2:
-    print("Yes")
-  elif X % 2 == 0:
-    print("No")
-  else:
-    X //= 2
-    print("Yes") if li[X] else print("No")
+main()
