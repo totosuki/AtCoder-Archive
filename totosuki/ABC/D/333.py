60db31458690b954650c937556e5cdca31751214
totosuki <e231kari@gmail.com>
2023-12-17 07:45:59 +0900

    [ABC 333] D Code Golf

--- totosuki/ABC/D/333.py
+++ totosuki/ABC/D/333.py
@@ -1,38 +1,19 @@
-from collections import defaultdict, deque
+# 152byte
+# 150byte
+from atcoder.dsu import*
+N=int(input())-1
+f=DSU(N+1)
+exec("u,v=map(int,input().split());u>1and f.merge(u,v);"*N)
+print(N-len(max(f.groups(),key=len)))
 
-def make_graph(N):
-  graph = defaultdict(list)
-  for _ in range(N-1):
-    u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
-  return graph
+# 149byte
+from atcoder.dsu import*;N,*A=open(0);N=int(N);D=DSU(N+1)
+for a in A:u,v=map(int,a.split());u>1and D.merge(u,v)
+print(N-len(max(D.groups(),key=len)))
 
-def solve(N, graph):
-  visited = [False] * (N+1)
-  visited[1] = True
-  rslt = []
-  for node in graph[1]:
-    cnt = 0
-    q = deque([node])
-    visited[node] = True
-    while q:
-      cnt += 1
-      now = q.pop()
-      next = graph[now]
-      for n in next:
-        if not visited[n]:
-          visited[n] = True
-          q.append(n)
-    rslt.append(cnt)
-  mx = max(rslt)
-  sm = sum(rslt)
-  print(sm - mx + 1)
-
-def main():
-  N = int(input())
-  graph = make_graph(N)
-  solve(N, graph)
-
-
-main()
+# 151byte
+# from atcoder.dsu import*
+# (N,),*A=[map(int,l.split())for l in open(0)]
+# f=DSU(N+1)
+# for u,v in A:u>1and f.merge(u,v)
+# print(N-len(max(f.groups(),key=len)))
