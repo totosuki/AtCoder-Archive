99fc3e5adfd84c851ed415bb1c5aedf6b2ba699a
totosuki <e231kari@gmail.com>
2024-01-10 11:14:09 +0900

    [鉄則本] A61 再AC

--- totosuki/etc/tessoku/A/61.py
+++ totosuki/etc/tessoku/A/61.py
@@ -1,15 +1,13 @@
-import sys; from collections import defaultdict
-input = sys.stdin.buffer.readline
+from collections import defaultdict
 
 N, M = map(int, input().split())
-graph = defaultdict(list)
+G = defaultdict(list)
 
 for _ in range(M):
   A, B = map(int, input().split())
-  graph[A].append(B)
-  graph[B].append(A)
+  G[A].append(str(B))
+  G[B].append(str(A))
 
 for k in range(1, N+1):
-  print(f"{k}:", end = " {")
-  print(*graph[k], sep = ", ", end = "")
-  print("}")
+  text = f"{k}: " + "{" + ", ".join(G[k]) + "}"
+  print(text)
